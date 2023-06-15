cecbc47e799465a80ecbd358ebd585f8aafcfb02
Becca Van Nostrand <vrs4he@virginia.edu>
2023-06-15 17:17:38 -0400

    changed binomial

--- Dist_plots.py
+++ Dist_plots.py
@@ -9,10 +9,10 @@ sns.set(style = "whitegrid")
 np.random.seed(34)
 
 fig, axs=plot.subplots(1,3,figsize = (15, 6))
-axs[0].hist(np.random.binomial(10, .7, 10000), color = 'red')
-axs[0].set_title("Binomial distribution with p=0.7")
+axs[0].hist(np.random.binomial(10, .5, 100000), color = 'red')
+axs[0].set_title("Binomial distribution with p=0.5")
 
-axs[1].hist(np.random.randn(10000), color = 'blue')
+axs[1].hist(np.random.randn(100000), color = 'blue')
 axs[1].set_title ("Normal Distribution")
 
 axs[2].hist(np.random.poisson(lam = 5, size = 10000), color ='green')
