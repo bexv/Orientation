0cfadaa810169af16879ab1cb276acd2106c31e9
Becca Van Nostrand <vrs4he@virginia.edu>
2023-06-15 17:12:39 -0400

    new file

---
+++ Dist_plots.py
@@ -0,0 +1,21 @@
+#Make histograms of popular probability distributions using a random number generator
+# use matplotlib and numpy
+
+import numpy as np
+import matplotlib.pyplot as plot
+import seaborn as sns
+
+sns.set(style = "whitegrid")
+np.random.seed(34)
+
+fig, axs=plot.subplots(1,3,figsize = (15, 6))
+axs[0].hist(np.random.binomial(10, .7, 10000), color = 'red')
+axs[0].set_title("Binomial distribution with p=0.7")
+
+axs[1].hist(np.random.randn(10000), color = 'blue')
+axs[1].set_title ("Normal Distribution")
+
+axs[2].hist(np.random.poisson(lam = 5, size = 10000), color ='green')
+axs[2].set_title ("Poisson distrbution")
+
+plot.show()
